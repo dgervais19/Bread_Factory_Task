b6324d4033506720679ef06f949157073649f731
Donovan Gervais <applemac@Donovans-MacBook-Pro-6.local>
2020-11-11 00:36:10 +0000

    factory testing code

--- bread_factory.py
+++ bread_factory.py
@@ -1,15 +1,33 @@
-# Create a class
-
+# import json to convert the arguments into a list
+import json
 class NaanFactory:
 
     def make_dough(self, *ingredients):
-        for items in ingredients:
-            ingredient = items.lower()
-            if ingredient == "water" and ingredient == "flour":
-                return "dough"
-            else:
-                return "You need flour and water to make dough"
+        ingredients_list = json.dumps(ingredients)
+        if "water" in ingredients_list and "flour" in ingredients_list:
+            return 'dough'
+        else:
+            return 'Both water and flour is needed'
+
+    def bake_bread(self, made_dough):
+        if made_dough == 'yes' or made_dough == 'Yes':
+            print("Great! now you can put it in the oven")
+            return 'naan'
+        else:
+            return 'make the dough'
+
+    def run_factory(self, baked_naan):
+        if baked_naan:
+            return 'factory is ready to run'
+        else:
+            return 'bake the naan'
 
 
-bread_factory = NaanFactory()
-print(bread_factory.make_dough("water", "flour"))
+
+
+
+if __name__ == '__main__':
+    bread_factory = NaanFactory()
+    print(bread_factory.make_dough('water', 'flour'))
+
+    print(bread_factory.bake_bread())
--- test_bread_factory.py
+++ test_bread_factory.py
@@ -1,2 +1,20 @@
+# import all the relative packages
 from bread_factory import NaanFactory
 import unittest
+import pytest
+
+# create a class to test the  methods
+class test_factory(unittest.TestCase):
+    factory = NaanFactory()
+
+    def test_make_dough(self):
+        self.assertEqual(self.factory.make_dough('water', 'flour'), 'dough')
+        self.assertEqual(self.factory.make_dough('water', 'egg'), 'Both water and flour is needed')
+
+    def test_bake_dough(self):
+        self.assertEqual(self.factory.bake_bread('Yes'), 'naan')
+        self.assertEqual(self.factory.bake_bread('no'), 'make the dough')
+
+    def test_run_factory(self):
+        self.assertEqual(self.factory.run_factory(True), 'factory is ready to run')
+        self.assertEqual(self.factory.run_factory(False), 'bake the naan')
